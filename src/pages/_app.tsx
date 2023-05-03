23ba9c183a133ede3dec5e103d1297517a4fabd0
songjihyun323 <88391843+songjihyun-dev@users.noreply.github.com>
2023-05-04 02:19:31 +0900

    🎉 initial commit @no-pla

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { RecoilRoot } from "recoil";
+import { ReactQueryDevtools } from "react-query/devtools";
+import Layout from "@/components/Layout/Layout";
+import "../style/reset.css";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+          <ReactQueryDevtools />
+        </QueryClientProvider>
+      </RecoilRoot>
+    </>
+  );
 }
