f7c74a54d7a401ab8a585fd03da8fc2d6487acf4
songjihyun323 <88391843+songjihyun-dev@users.noreply.github.com>
2023-05-05 05:14:34 +0900

    ♻️ 유저 정보 불러오는 코드 리팩토링 #1 @no-pla

--- src/pages/banking/index.tsx
+++ src/pages/banking/index.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import useUser from "@/components/Hooks/useUser";
+import React, { useEffect } from "react";
+import { useQueryClient } from "react-query";
 
-const Index = () => {
-  return <div>Index</div>;
+const Index = ({ currentUser }: any) => {
+  const queryClient = useQueryClient();
+  const userInfo = useUser(currentUser?.uid);
+
+  useEffect(() => {
+    queryClient.invalidateQueries("getReadBookInfo");
+  }, []);
+
+  return <div>{userInfo?.length.toLocaleString("ko-KR")}권</div>;
 };
 
 export default Index;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,17 +6,22 @@ export default function Home({ currentUser }: any) {
   const queryClient = useQueryClient();
   const userInfo = useUser(currentUser?.uid);
 
-  console.log("메인 리렌더");
-
   useEffect(() => {
-    queryClient.invalidateQueries("getUserInfo");
+    queryClient.invalidateQueries("getReadBookInfo");
   }, []);
 
   return (
-    <h1>
-      {currentUser?.displayName || "닉네임"}
-      <br />
-      {userInfo?.totalBook || 0}권
-    </h1>
+    <>
+      <div>{currentUser?.displayName || "닉네임"}</div>
+      <div>{userInfo?.length.toLocaleString("ko-KR")}권</div>
+      <div>
+        {userInfo
+          ?.reduce((cur: number, acc: any) => {
+            return cur + acc.price;
+          }, 0)
+          .toLocaleString("ko-KR")}
+        원
+      </div>
+    </>
   );
 }
