c732701a16373e76cdedfda04a8a720e9f14eff8
songjihyun323 <88391843+songjihyun-dev@users.noreply.github.com>
2023-05-21 06:12:32 +0900

    💄 메인 화면 스타일 수정 #17 @no-pla

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,23 +11,24 @@ export default function Home({ currentUser }: any) {
   const queryClient = useQueryClient();
   const userInfo = useUser(currentUser?.uid);
 
-  const { data: keywords } = useQuery(
-    "getMonthlyKeyword",
-    async () => {
-      return await axios.get(
-        `http://data4library.kr/api/monthlyKeywords?authKey=${
-          process.env.NEXT_PUBLIC_BIG_DATA_KEY
-        }&month=${
-          new Date().getFullYear() +
-          "-" +
-          String(new Date().getMonth()).padStart(2, "0")
-        }&format=json`
-      );
-    },
-    {
-      select: (data) => data?.data.response.keywords.slice(0, 5),
-    }
-  );
+  // API 1일 500회 제한이므로 잠시 주석처리함
+  // const { data: keywords } = useQuery(
+  //   "getMonthlyKeyword",
+  //   async () => {
+  //     return await axios.get(
+  //       `http://data4library.kr/api/monthlyKeywords?authKey=${
+  //         process.env.NEXT_PUBLIC_BIG_DATA_KEY
+  //       }&month=${
+  //         new Date().getFullYear() +
+  //         "-" +
+  //         String(new Date().getMonth()).padStart(2, "0")
+  //       }&format=json`
+  //     );
+  //   },
+  //   {
+  //     select: (data) => data?.data.response.keywords.slice(0, 5),
+  //   }
+  // );
 
   useEffect(() => {
     queryClient.invalidateQueries("getReadBookInfo");
@@ -45,39 +46,39 @@ export default function Home({ currentUser }: any) {
             height={100}
             width={100}
             alt={`${currentUser?.displayName} 님의 프로필 사진입니다.`}
-            style={{ borderRadius: "50%" }}
           />
-          <div>{currentUser?.displayName || "닉네임 없음"}</div>
+          <UserName>{currentUser?.displayName || "닉네임 없음"}</UserName>
+          <Link href="/user/setting">프로필 설정</Link>
         </UserInfo>
         <BankingInfo>
-          <div>
+          <BankName>
             {currentUser?.displayName || "닉네임 없음"}&nbsp;님의 독서 통장
-          </div>
-          <div>
+          </BankName>
+          <BankAmount>
             {userInfo
               ?.reduce((cur: number, acc: any) => {
                 return cur + acc.price;
               }, 0)
               .toLocaleString("ko-KR") || 0}
-            원
-          </div>
-          <div>
+          </BankAmount>
+          <BankPage>
             <Link href="/banking">내역 보기</Link>
             <Link href="/banking/deposit">입금하기</Link>
-          </div>
+          </BankPage>
         </BankingInfo>
-        <UserInfo>
-          <div>최근 인기 도서 키워드</div>
-          <ul>
-            {keywords?.map(({ keyword }: any, index: number) => {
+        {/* API 횟수 제한으로 임시 주석 처리 */}
+        <RankingInfo>
+          <RankingTitle>최근 인기 도서 키워드</RankingTitle>
+          <RankingList>
+            {/* {keywords?.map(({ keyword }: any, index: number) => {
               return (
                 <li key={index}>
                   {index + 1}.&nbsp;{keyword?.word}
                 </li>
               );
-            })}
-          </ul>
-        </UserInfo>
+            })} */}
+          </RankingList>
+        </RankingInfo>
       </InfoContainer>
       <Chart currentUser={currentUser} />
     </Container>
@@ -107,21 +108,33 @@ const InfoContainer = styled.section`
 
 const UserInfo = styled.section`
   width: 20vw;
-  background-color: var(--main-color);
+  background-color: var(--sub-main-color);
   display: flex;
   flex-direction: column;
   align-items: center;
   box-sizing: border-box;
   border-radius: 12px;
   height: 200px;
+  > img {
+    border-radius: 50%;
+    margin: 20px 0;
+  }
+  a {
+    font-size: 0.9rem;
+  }
   @media (max-width: 600px) {
     width: 100%;
   }
 `;
 
+const UserName = styled.div`
+  margin-bottom: 16px;
+  font-weight: 800;
+`;
+
 const BankingInfo = styled.section`
   width: 60vw;
-  background-color: var(--main-color);
+  background-color: var(--sub-main-color);
   box-sizing: border-box;
   border-radius: 12px;
   display: flex;
@@ -132,4 +145,59 @@ const BankingInfo = styled.section`
     width: 100%;
     height: 200px;
   }
+`;
+
+const BankName = styled.div`
+  width: 100%;
+  box-sizing: border-box;
+  padding: 0 20px;
+  font-weight: 700;
+`;
+
+const BankAmount = styled.span`
+  font-weight: 800;
+  font-size: 1.6rem;
+  &::after {
+    content: "원";
+  }
+`;
+
+const BankPage = styled.div`
+  > a {
+    cursor: pointer;
+  }
+  > a:first-of-type::after {
+    content: "|";
+    padding: 0 12px;
+  }
+`;
+
+const RankingInfo = styled.section`
+  width: 20vw;
+  background-color: var(--sub-main-color);
+  border-radius: 20px;
+  padding: 12px;
+  box-sizing: border-box;
+  text-align: center;
+  @media (max-width: 600px) {
+    width: 100%;
+  }
+`;
+
+const RankingTitle = styled.div`
+  font-weight: 800;
+  margin: 8px 0;
+`;
+
+const RankingList = styled.ul`
+  background-color: #faf9fb;
+  margin-top: 4px;
+  height: 84%;
+  border-radius: 12px;
+  padding: 12px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  text-align: left;
 `;
